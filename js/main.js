851041e41d0c072195b2b37542b44410ed54ab69
mancillasroberto <mancillasroberto@fullsail.edu>
2016-08-26 23:29:42 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+
+	$( "#accordion:nth-child(1n)" ).accordion({
+		active: false,
+      	collapsible: true,
+		icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+	});
+
+
+
+});
